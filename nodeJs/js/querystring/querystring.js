59def4bd74c9ba15ee54d95103ff68c34e8bd8cb
kimhankyeol <kimhk1030@naver.com>
2019-01-05 19:13:41 +0900

    2019-01-05 19:13 queryString

--- nodeJs/js/querystring/querystring.js
+++ nodeJs/js/querystring/querystring.js
@@ -5,5 +5,9 @@ const querystring = require('querystring')
 
 const parsedUrl = url.parse('http://www.gilbut.co.kr/?page=3&limit=10&category=nodejs&category=javascript');
 const query = querystring.parse(parsedUrl.query);
+
+//querystring.parse(쿼리): url 의 query 부분을 자바스크립트 객체로 분해해줌
 console.log('querystring.parse() :', query);
+
+//querystring.stringify(객체) : 분해된 query 객체를 문자열로 다시 조립해줍니다.
 console.log('querystring.stringify():',querystring.stringify(query));
