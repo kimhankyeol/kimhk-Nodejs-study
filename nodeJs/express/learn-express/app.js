0e757f445ab5e7972b280544c3f18a2c84ad4b1b
kimhankyeol <kimhk1030@naver.com>
2019-01-16 11:40:34 +0900

    2019-01-16 11:40 templateEngine

--- nodeJs/express/learn-express/app.js
+++ nodeJs/express/learn-express/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs'); // pug, ejs
 
 //custom middleware
 app.use(function(req,res,next){
